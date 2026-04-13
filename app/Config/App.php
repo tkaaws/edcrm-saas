864e6a3989e98e1a26abc9a8cd1da163a14d523e
kirandigrase2018 <kirandigrase2018@gmail.com>
2026-04-13 18:50:45 +0530

    chore: set droplet base URL for initial deploy

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost:8080/';
+    public string $baseURL = 'http://143.110.247.79/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
